ba79dc79c1cb6cb54d947e779bdfe5e57a0f0a2c
Andrew Ziem <ahz001@gmail.com>
2014-03-18 20:38:01 -0600

    Add merge-commit.sh

---
+++ merge-commit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright (C) 2014 by Andrew Ziem.  All rights reserved.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+#
+# Generate Winapp2-combined.ini
+#
+
+echo git pull
+git pull
+
+echo cat
+cat Winapp2.ini removed-entries.ini > Winapp2-combined.ini
+
+echo Removing Default= lines, which are not used by BleachBit
+grep -viP "^Default=" Winapp2-combined.ini >Winapp2-combined.tmp
+
+echo Counting
+ENTRY_COUNT=`grep "\[" Winapp2-combined.tmp | wc -l`
+echo $ENTRY_COUNT entries
+
+echo Creating header
+echo "; Winapp2.ini for BleachBit" > Winapp2-combined.header
+echo ";  A combination of www.winapp2.com's winapp2.ini plus extra entries" >> Winapp2-combined.header
+echo ";  $ENTRY_COUNT entries" >> Winapp2-combined.header
+echo ";  https://github.com/az0/winapp2.ini/" >> Winapp2-combined.header
+echo " " >> Winapp2-combined.header
+echo " " >> Winapp2-combined.header
+
+echo Combining body with header
+cat Winapp2-combined.header Winapp2-combined.tmp > Winapp2-combined.ini
+rm Winapp2-combined.{header,tmp}
+
+echo git commit (in 5 seconds)
+sleep 5
+git commit -m 'Automatic update by combining Winapp2.ini plus removed-entries.ini' Winapp2-combined.ini
+
+echo git push
+git push

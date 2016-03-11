791b77ca2a331a6477bb740eb54caf53ee6e6574
Andrew Ziem <ahz001@gmail.com>
2016-03-10 21:13:12 -0700

    Initial commit of profiling script

---
+++ profile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright (C) 2016 by Andrew Ziem.  All rights reserved.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+#
+# Statistics
+#
+
+echo The distribution of FileKey## as it relates to MAX_DETECT
+grep -iPo "filekey\d+" Winapp2.ini  | grep -oP "\d+" | sort -n |uniq -c
+
+echo
+echo Environment variables
+grep -iPo "%[^%]+%" Winapp2-combined.ini  | sort | uniq -ci
